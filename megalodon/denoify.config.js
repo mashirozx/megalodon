a289b43263ca7883f763782160e80e3ddf6f2b6e
mashirozx <moezhx@outlook.com>
2023-04-30 22:44:37 +0800

    denoify

---
+++ megalodon/denoify.config.js
@@ -0,0 +1,10 @@
+// @ts-check
+
+/** @type { import('denoify/lib/config/parseParams').DenoifyParams } */
+const config = {
+	"ports": {
+		"axios": "https://deno.land/x/axiod/mod.ts"
+	},
+ }
+ 
+ module.exports = config;
